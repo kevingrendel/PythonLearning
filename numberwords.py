b730ced58a1cb2710c9e395f171c387c6451d52f
kevingrendel <kevin_grendel@yahoo.com>
2017-11-24 11:14:22 -0500

    numbers up through trillions

--- numberwords.py
+++ numberwords.py
@@ -20,15 +20,39 @@ def get20to99(num):
     numStr = str(num)
     tens = numStr[0]
     s = numWords[int(tens) - 2]
-    s = s + getTens(int(numStr[1]))
+    s = s + " " + getTens(int(numStr[1]))
     return s
 
-def getWordFromNumber(num):
+def getBase10(num, word, base):
+    baseNum = num // base
+    word = getWordFromRanges(baseNum) + word + getWordFromRanges(num - (baseNum*base))
+    return word
+
+def getWordFromRanges(num):
+    word = ""
     if num < 20:
         word = getUnderTwenty(num)
     elif num >= 20 and num < 100:
         word = get20to99(num)
+    elif num >= 100 and num < 1000:
+        word = getBase10(num, " hundred ", 100)
+    elif num >= 1000 and num < 1000000:
+        word = getBase10(num, " thousand ", 1000)
+    elif num >= 1000000 and num < 1000000000:
+        word = getBase10(num, " million ", 1000000)
+    elif num >= 1000000000 and num < 1000000000000:
+        word = getBase10(num, " billion ", 1000000000)
+    elif num >= 1000000000000 and num < 1000000000000000:
+        word = getBase10(num, " trillion ", 1000000000000)
+    else:
+        word = "too big!"
+
+    if len(word) > 4 and word.endswith("zero"):
+        word = word.replace("zero", "")
     return word
+
+def getWordFromNumber(num):
+    return getWordFromRanges(num)
 
 
 while(True):
